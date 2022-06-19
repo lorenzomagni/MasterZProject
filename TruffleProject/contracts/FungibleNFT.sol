d9065c620cf804f56fc4aa1f4ae80bcf2306169b
Lorenzo <lorenzomagni1997@gmail.com>
2022-06-19 16:20:24 +0200

    Progressed with the development of RFT

--- TruffleProject/contracts/FungibleNFT.sol
+++ TruffleProject/contracts/FungibleNFT.sol
@@ -4,12 +4,12 @@ import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract FungibleNFT is ERC20 {
+contract RFT is ERC20 {
     uint public icoSharePrice;
     uint public icoShareSupply;
 
     uint public nftId;
-    IERC821 public nft;
+    IERC721 public nft;
     IERC20 public dai;
 
     address public admin;
@@ -18,5 +18,48 @@ contract FungibleNFT is ERC20 {
         string memory _name,
         string memory _symbol,
         address _nftAddress,
+        uint _nftId,
+        uint _icoSharePrice,
+        uint _icoShareSupply,
+        address _daiAddress
     )
+    ERC20(_name, _symbol)
+    {
+        nftId = _nftId;
+        nft = IERC721(_nftAddress);
+        icoSharePrice = _icoSharePrice;
+        icoShareSupply = _icoShareSupply;
+        dai = IERC20(_daiAddress);
+        admin = msg.sender;
+    }
+
+    function startIco() external {
+        require(msg.sender == admin, 'only admin');
+        nft.transferFrom(msg.sender, address(this), nftId);
+        //uint icoEnd = block.timestamp + 7 * 86400;
+    }
+
+    function buyShare(uint shareAmount) external {
+        uint icoEnd = block.timestamp + 7 * 86400;
+        require(icoEnd > 0, 'ICO not started yet');
+        require(block.timestamp <= icoEnd, 'ICO is finished');
+        require(totalSupply() + shareAmount <= icoShareSupply, 'not enough shares left');
+        uint daiAmount = shareAmount * icoSharePrice;
+        dai.transferFrom(msg.sender, address(this), daiAmount);
+        _mint(msg.sender, shareAmount);
+    }
+
+    function withdrawProfits() external {
+        uint icoEnd = block.timestamp + 7 * 86400;
+        require(msg.sender == admin, 'only admin');
+        require(block.timestamp > icoEnd, 'ICO not finished yet');
+        uint daiBalance = dai.balanceOf(address(this));
+        if(daiBalance > 0) {
+            dai.transfer(admin, daiBalance);
+        }
+        uint unsoldShareBalance = icoShareSupply - totalSupply();
+        if(unsoldShareBalance > 0) {
+            _mint(admin, unsoldShareBalance);
+        }
+    }
 }
