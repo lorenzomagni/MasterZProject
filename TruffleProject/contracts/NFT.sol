cbafa0ead9f7ddcadf28259ee798a97846a9bedf
Lorenzo <lorenzomagni1997@gmail.com>
2022-06-20 22:30:45 +0200

    major updates

--- TruffleProject/contracts/NFT.sol
+++ TruffleProject/contracts/NFT.sol
@@ -1,6 +1,7 @@
-pragma solidity ì0.7.3;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
 
-import 'openzeppelin/contracts/token/ERC721/ERC721.sol;
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 contract NFT is ERC721 {
     constructor(
